a59cd9759617e910c917e13f5e4b5e2a4fa5b1a7
Your Name <shashank@gmail.com>
2023-01-07 13:52:42 +0545

    initial commit

---
+++ nested class/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace MyApplication
+{
+    class car
+    {
+        string color = "red";
+        static void Main(string[] args)
+        {
+            car myobj = new car(); ;
+            Console.WriteLine(myobj.color);
+        }
+    }
+}
